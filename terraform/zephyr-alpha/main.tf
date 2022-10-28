e47d0f211eea049dd08a192c03a8d5f84dfd17cb
Stephanos Ioannidis <root@stephanos.io>
2022-10-28 23:36:02 +0900

    zephyr-alpha: Add kubectl provider
    This commit adds the Terraform kubectl provider to the cluster
    configurations to facilitate applying kubectl manifest (k8s YAML files)
    directly from the Terraform configurations.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- terraform/zephyr-alpha/main.tf
+++ terraform/zephyr-alpha/main.tf
@@ -8,6 +8,12 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
+provider "kubectl" {
+  host                   = module.eks_blueprints.eks_cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
 provider "helm" {
   kubernetes {
     host                   = module.eks_blueprints.eks_cluster_endpoint
