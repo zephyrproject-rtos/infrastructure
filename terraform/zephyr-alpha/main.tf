8c1d15945683753186117cc1590eb7f31472988b
Stephanos Ioannidis <root@stephanos.io>
2022-10-30 02:53:13 +0900

    zephyr-alpha: Add zephyr-alpha based on zephyr-aws-blueprints
    This commit adds new `zephyr-alpha` deployment configurations using the
    refactored `zephyr-aws-blueprints` configuration template.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

---
+++ terraform/zephyr-alpha/main.tf
@@ -0,0 +1,42 @@
+module "zephyr_aws_blueprints" {
+  source = "../zephyr-aws-blueprints"
+
+  cluster_name = "zephyr-alpha"
+  aws_region   = "us-east-2"
+
+  aws_auth_map_users = [
+    {
+      userarn  = "arn:aws:iam::724087766192:user/terraform-cloud"
+      username = "terraform-cloud"
+      groups   = ["system:masters"]
+    },
+    {
+      userarn  = "arn:aws:iam::724087766192:user/stephanosio"
+      username = "stephanosio"
+      groups   = ["system:masters"]
+    }
+  ]
+
+  mng_od_8vcpu_16mem_min_size     = 1
+  mng_od_8vcpu_16mem_max_size     = 10
+  mng_od_8vcpu_16mem_desired_size = 2
+
+  mng_spot_4vcpu_8mem_min_size     = 0
+  mng_spot_4vcpu_8mem_max_size     = 100
+  mng_spot_4vcpu_8mem_desired_size = 1
+
+  mng_spot_16vcpu_32mem_min_size     = 0
+  mng_spot_16vcpu_32mem_max_size     = 100
+  mng_spot_16vcpu_32mem_desired_size = 1
+
+  github_organization = "zephyrproject-rtos"
+
+  kube_prometheus_stack_grafana_password = var.kube_prometheus_stack_grafana_password
+
+  actions_runner_controller_github_app_id              = "250859"
+  actions_runner_controller_github_app_installation_id = "30394780"
+  actions_runner_controller_github_app_private_key     = var.actions_runner_controller_github_app_private_key
+
+  actions_runner_controller_webhook_server_host   = "webhook.arc-alpha.ci.zephyrproject.io"
+  actions_runner_controller_webhook_server_secret = var.actions_runner_controller_webhook_server_secret
+}
---
+++ terraform/zephyr-alpha/variables.tf
@@ -0,0 +1,17 @@
+variable "kube_prometheus_stack_grafana_password" {
+  description = "Grafana password for Kube Prometheus Stack"
+  type        = string
+  sensitive   = true
+}
+
+variable "actions_runner_controller_github_app_private_key" {
+  description = "GitHub app private key for Actions Runner Controller"
+  type        = string
+  sensitive   = true
+}
+
+variable "actions_runner_controller_webhook_server_secret" {
+  description = "Webhook server secret for Actions Runner Controller"
+  type        = string
+  sensitive   = true
+}
