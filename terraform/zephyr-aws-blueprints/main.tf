6e9f8c097edbca6a2c02bdece49ef0bb65b96fd8
Stephanos Ioannidis <root@stephanos.io>
2022-10-31 04:41:00 +0900

    zephyr-aws-blueprints: Do not load config file for kubectl
    Loading kubectl config file leads to an "apply" failure on the
    Terraform cloud.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- terraform/zephyr-aws-blueprints/main.tf
+++ terraform/zephyr-aws-blueprints/main.tf
@@ -12,6 +12,7 @@ provider "kubectl" {
   host                   = module.eks_blueprints.eks_cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
+  load_config_file       = false
 }
 
 provider "helm" {
