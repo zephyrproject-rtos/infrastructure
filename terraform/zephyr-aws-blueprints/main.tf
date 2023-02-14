e66f40dcba48639181c9b27999be861876aa5a69
Stephanos Ioannidis <root@stephanos.io>
2023-02-15 08:18:02 +0900

    zephyr-aws-blueprints: Use zephyr-runner AMI 2023-02-14
    This commit updates the zephyr-runner deployment to use the AMI
    2023-02-14, which includes the CI docker image v0.24.11.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- terraform/zephyr-aws-blueprints/main.tf
+++ terraform/zephyr-aws-blueprints/main.tf
@@ -47,7 +47,7 @@ data "aws_ami" "zephyr_runner_node_x86_64" {
 
   filter {
     name   = "name"
-    values = ["zephyr-runner-node-x86_64-1669225251"]
+    values = ["zephyr-runner-node-x86_64-1676379163"]
   }
 
   owners = ["724087766192"]
@@ -58,7 +58,7 @@ data "aws_ami" "zephyr_runner_node_arm64" {
 
   filter {
     name   = "name"
-    values = ["zephyr-runner-node-arm64-1669228883"]
+    values = ["zephyr-runner-node-arm64-1676381580"]
   }
 
   owners = ["724087766192"]
