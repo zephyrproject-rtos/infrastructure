5f71fc440d9f02a317e957b656094ab7aa0f6056
Stephanos Ioannidis <root@stephanos.io>
2024-02-16 17:11:49 +0900

    zephyr-runner-v2: zephyr-runner-aws: Set cloud information as envvars
    This commit adds the runner environment variables that provide the
    underlying cloud service information for debugging and cloud-specific
    workflow handling purposes.
    
    As part of this, it adds the pod templates for instantiating the
    workflow containers, which is necessary in order to make the cloud
    service information environment variables available inside containers.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- terraform/aws-zephyr-alpha/versions.tf
+++ terraform/aws-zephyr-alpha/versions.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.4.1"
