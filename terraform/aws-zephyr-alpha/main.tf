6cce0326b410e821fc86a59363da27d3e0a4b3b1
Stephanos Ioannidis <root@stephanos.io>
2024-06-13 21:51:38 +0900

    aws-zephyr-alpha: Remove zephyr-runner v1 resources
    This commit removes all zephyr-runner v1-related resources from the
    aws-zephyr-alpha deployment.
    
    Signed-off-by: Stephanos Ioannidis <root@stephanos.io>

--- terraform/aws-zephyr-alpha/main.tf
+++ terraform/aws-zephyr-alpha/main.tf
@@ -67,19 +67,10 @@ module "zephyr_aws_blueprints" {
 
   kube_prometheus_stack_grafana_password = data.hcp_vault_secrets_app.zephyr_secrets.secrets["kube_prometheus_stack_grafana_password"]
 
-  actions_runner_controller_github_app_id              = nonsensitive(data.hcp_vault_secrets_app.zephyr_secrets.secrets["zephyr_runner_github_app_id"])
-  actions_runner_controller_github_app_installation_id = nonsensitive(data.hcp_vault_secrets_app.zephyr_secrets.secrets["zephyr_runner_github_app_installation_id"])
-  actions_runner_controller_github_app_private_key     = data.hcp_vault_secrets_app.zephyr_secrets.secrets["zephyr_runner_github_app_private_key"]
-
   actions_runner_controller_v2_version                    = local.arc_version
   actions_runner_controller_v2_github_app_id              = data.hcp_vault_secrets_app.zephyr_secrets.secrets["zephyr_runner_github_app_id"]
   actions_runner_controller_v2_github_app_installation_id = data.hcp_vault_secrets_app.zephyr_secrets.secrets["zephyr_runner_github_app_installation_id"]
   actions_runner_controller_v2_github_app_private_key     = data.hcp_vault_secrets_app.zephyr_secrets.secrets["zephyr_runner_github_app_private_key"]
-
-  enable_zephyr_runner_linux_x64_xlarge = true
-  enable_zephyr_runner_linux_x64_4xlarge = true
-  enable_zephyr_runner_linux_arm64_xlarge = true
-  enable_zephyr_runner_linux_arm64_4xlarge = true
 }
 
 # Actions Runner Controller (ARC) Runner Scale Sets
